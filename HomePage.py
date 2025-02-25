a6be6429812f37f4729eafa71ad8d0f5cdd7aa50
Patrick Knowles <pgknowles1@gmail.com>
2025-02-24 20:55:38 -0600

    WIP banner

--- HomePage.py
+++ HomePage.py
@@ -1,6 +1,11 @@
 # This script will define the Home Page section of the new application
 
 import streamlit as st
+import streamlit.components.v1 as components
+from PIL import Image
+
+wip = Image.open("./Images/WIP.jpeg")
 
 def HomePage():
     st.subheader("The Alternative F1 League")
+    st.image(wip)
