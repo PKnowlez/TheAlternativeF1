d4e3ca691beb31b4c81891667d317c107d1c70ea
Patrick Knowles <pgknowles1@gmail.com>
2025-02-24 20:56:38 -0600

    WIP

--- HomePage.py
+++ HomePage.py
@@ -4,8 +4,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
 
-wip = Image.open("./Images/WIP.jpeg")
-
 def HomePage():
     st.subheader("The Alternative F1 League")
-    st.image(wip)
+    st.makrdown('''
+                Work In Progress
+                ''')
