710fd8afb4b4d4b5656d4064717838654bca57d7
Patrick Knowles <pgknowles1@gmail.com>
2025-02-24 20:57:37 -0600

    typo 2

--- HomePage.py
+++ HomePage.py
@@ -1,8 +1,6 @@
 # This script will define the Home Page section of the new application
 
 import streamlit as st
-import streamlit.components.v1 as components
-from PIL import Image
 
 def HomePage():
     st.subheader("The Alternative F1 League")
