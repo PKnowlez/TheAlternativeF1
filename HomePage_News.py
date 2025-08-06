a9e3858efda3271ad695306342c3a7f93b84495e
Patrick Knowles <pgknowles1@gmail.com>
2025-08-05 22:46:04 -0500

    carousel update

--- HomePage_News.py
+++ HomePage_News.py
@@ -2,6 +2,20 @@ import streamlit as st
 from PIL import Image
 from streamlit_carousel import carousel
 
+st.markdown(
+    """
+    <style>
+    /* Target the image container within the carousel */
+    .stCarousel img {
+        width: 100%; /* Make image fill the container width */
+        height: auto; /* Maintain the image's aspect ratio */
+        object-fit: contain; /* Ensure the image is fully visible without cropping */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 def HomePageNews():
     #region Driver Announcements
     driver_announcements = [
@@ -10,8 +24,13 @@ def HomePageNews():
             "text": "",
             "img": "./Images/Alpine_Driver_Post.png"
         },
+        {
+            "title": "",
+            "text": "",
+            "img": "./Images/Alpine_Driver_Post.png"
+        },
     ]
 
-    carousel(driver_announcements)
+    carousel(items=driver_announcements, autoplay=True, interval=30000)
     st.divider()
     #endregion
