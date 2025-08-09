cdec7a954839867b80502143e1f98bb2dd4c04b7
Patrick Knowles <pgknowles1@gmail.com>
2025-08-09 18:46:10 -0500

    adding latest news section

--- HomePage_News.py
+++ HomePage_News.py
@@ -2,75 +2,86 @@ import streamlit as st
 from PIL import Image
 from streamlit_carousel import carousel
 
-# st.markdown(
-#     """
-#     <style>
-#     /* Target the carousel's slide item container */
-#     .slick-slide img {
-#         width: 100%;
-#         height: auto;
-#         object-fit: contain;
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-# )
-
 def HomePageNews():
-    #region Driver Announcements
-    driver_announcements = [
+    #region Latest News
+    latest_news = [
         {
             "title": "",
             "text": "",
             "img": "./Images/Driver_Lineup.png"
         },
-        {
-            "title": "",
-            "text": "",
-            "img": "./Images/Alpine_Driver_Post.png"
-        },
-        {
-            "title": "",
-            "text": "",
-            "img": "./Images/McLaren_Driver_Post.png"
-        },
-        {
-            "title": "",
-            "text": "",
-            "img": "./Images/Red_Bull_Driver_Post.png"
-        },
-        {
-            "title": "",
-            "text": "",
-            "img": "./Images/VCARB_Driver_Post.png"
-        },
-        {
-            "title": "",
-            "text": "",
-            "img": "./Images/Intern_1.png"
-        },
-        {
-            "title": "",
-            "text": "",
-            "img": "./Images/Intern_2.png"
-        },
-        {
-            "title": "",
-            "text": "",
-            "img": "./Images/Aston_Martin_Driver_Post.png"
-        },
-        {
-            "title": "",
-            "text": "",
-            "img": "./Images/Ferrari_Driver_Post.png"
-        },
-        {
-            "title": "",
-            "text": "",
-            "img": "./Images/Mercedes_Driver_Post.png"
-        },
     ]
-
-    carousel(items=driver_announcements, interval=20000)
+    
+    carousel(items=latest_news, interval=20000)
     st.divider()
     #endregion
+
+    # ----------------------------------------------------------------------------------------------------------
+    # "Show More/Less" button 
+    if not st.session_state.show_all_content:
+        if st.button('Show More'):
+            st.session_state.show_all_content = True
+            st.rerun()  # Rerun the app to show everything
+    else: 
+        if st.button('Show Less'):
+            st.session_state.show_all_content = False
+            st.rerun()
+
+    if st.session_state.show_all_content:
+    #region Driver Announcements
+        driver_announcements = [
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/Driver_Lineup.png"
+            },
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/Alpine_Driver_Post.png"
+            },
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/McLaren_Driver_Post.png"
+            },
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/Red_Bull_Driver_Post.png"
+            },
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/VCARB_Driver_Post.png"
+            },
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/Intern_1.png"
+            },
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/Intern_2.png"
+            },
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/Aston_Martin_Driver_Post.png"
+            },
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/Ferrari_Driver_Post.png"
+            },
+            {
+                "title": "",
+                "text": "",
+                "img": "./Images/Mercedes_Driver_Post.png"
+            },
+        ]
+
+        carousel(items=driver_announcements, interval=20000)
+        st.divider()
+        #endregion
