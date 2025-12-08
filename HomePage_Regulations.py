62950ae27c75ceb200d1b23a5d923c809c909843
Patrick Knowles <pgknowles1@gmail.com>
2025-12-07 18:29:56 -0700

    regulations update

--- HomePage_Regulations.py
+++ HomePage_Regulations.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def HomePageRegulations():
     rules = {
-        "":             ['1','2','3','4','5','6','7','8','9','10','11','12'],
+        "":             ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'],
         "Regulation":   [
                             'Points Elgibility',
                             'Finishing Points',
@@ -17,7 +17,10 @@ def HomePageRegulations():
                             'Right to Protest',
                             'Penalty Points',
                             'Sprint Day Format',
-                            'Sprint Race Finishing Points'
+                            'Sprint Race Finishing Points',
+                            'Race Start Incident',
+                            'Causing a Collision',
+                            'Race Restarts'
                         ],
         "Definition":   [
                             'Points are only awarded to drivers that complete the race. No points are awarded to those who DNS or DNF.',
@@ -31,7 +34,10 @@ def HomePageRegulations():
                             'Any driver that disagrees with a ruling, has the right to protest the ruling within one day of the final ruling. This protest will be reviewed by the league and require a 2/3rds majority vote to overturn.',
                             "Drivers who earn an Endangering or Ruining Another Driver's Race penalty will also earn a penalty point. If a driver earns two (2) penalty points, their next offense will increase the severity by an additional two (2) places. For every additional 2 penalty points after that, the place penalty will increase by three (3) places.",
                             'Sprint Qualifying > Sprint > Race Qualifying = Reverse Grid set by Sprint Results (All AI will be placed in front of ALL drivers, regardless of finishing position)',
-                            'Points are awarded based on finishing postion in each race and follow the following pattern: 1st - 8, 2nd - 7, 3rd - 6, 4th - 5, 5th - 4, 6th - 3, 7th - 2, 8th - 1, 9th to 20th - 0.5.'
+                            'Points are awarded based on finishing postion in each race and follow the following pattern: 1st - 8, 2nd - 7, 3rd - 6, 4th - 5, 5th - 4, 6th - 3, 7th - 2, 8th - 1, 9th to 20th - 0.5.',
+                            "During the start of a race or Red Flag restart, any driver found to be the cause of an incident will be penalized in the next main race they participate in.** An incident in this scenario is any collision, squeeze, or brake check, involving one or more cars getting damage or losing more than 3 places due to the incident. The driver at fault will be penalized by being unable to compete in Q2 of the next race. In other words, the driver will qualify last, unless there are late drivers, in which those drivers that are late will start behind the offending driver from the previous race. A penalty point will also be added to the driver's super license for the remainder of the season.",
+                            "In some scenarios, drivers may cause a collision that is not severe enough to meet the requirements of Regulation 8 - Endangering or Ruining Another Driver's Race. In these scenarios, drivers who cause a collision will be penalized with a 5 second or 10 second penalty to their finishing position. The size of the penalty will be based on league review and severity of the incident. A penalty point will also be added to the driver's super license for remainder of the season.",
+                            'Races will not be restarted for racing incidents. If there is a bug or glitch during or before the race start, the race may be restarted depending on the scenario.'
                         ]
     }
     df = pd.DataFrame(rules)
@@ -50,5 +56,8 @@ def HomePageRegulations():
 
     st.markdown(html_table, unsafe_allow_html=True)
     st.markdown(
-        f""" <p style="color:lightgray;">*This regulation only applies after the first 2 laps of the race or restart. If reckless driving occurs during a start or restart, the incident will be reviewed per request of the driver who's race was endangered or ruined.</p>
+        f""" <p style="color:lightgray;">
+        *This regulation only applies after the first 2 laps of the race or restart. If reckless driving occurs during a start or restart, the incident will be reviewed per request of the driver who's race was endangered or ruined.
+        **This regulation will be modified for the final race of the season to penalize the driver on their finishing position in the final race, rather than carrying over to the next season.
+        </p>
         """, unsafe_allow_html=True,)
